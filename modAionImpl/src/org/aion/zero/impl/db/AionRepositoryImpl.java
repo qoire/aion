2c05840f3cbf173c9494336c7f71023e53ec5840
Alexandra Roatis <alexandra@nuco.io>
2019-06-28 16:35:12 -0400

    consensus test for debugging

--- modAionImpl/src/org/aion/zero/impl/db/AionRepositoryImpl.java
+++ modAionImpl/src/org/aion/zero/impl/db/AionRepositoryImpl.java
@@ -1123,6 +1123,7 @@ public class AionRepositoryImpl
 
     /** Used when the codeHash is already known. */
     public InternalVmType getVMUsed(AionAddress contract, byte[] codeHash) {
+        System.out.println("getVM: " + contract + " code: " + Hex.toHexString(codeHash));
         if (ContractFactory.isPrecompiledContract(contract)) {
             // skip the call to disk
             return InternalVmType.FVM;
